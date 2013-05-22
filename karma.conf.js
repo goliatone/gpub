0652bd55a6c10e38036c465b93e50f5682286ead
goliatone <burgosemi@gmail.com>
2013-05-22 09:05:34 -0400

    Added sinon and matchers to test. Updated karma config.

--- karma.conf.js
+++ karma.conf.js
@@ -8,17 +8,20 @@ basePath = '';
 
 // list of files / patterns to load in the browser
 files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  REQUIRE,
-  REQUIRE_ADAPTER,
+  	JASMINE,
+  	JASMINE_ADAPTER,
+  	REQUIRE,
+  	REQUIRE_ADAPTER,
 
-  {pattern: 'libs/jquery/jquery.js', included: false},
-  {pattern: 'libs/requirejs/require.js', included: false},
-  {pattern: 'src/*.js', included: false},
-  {pattern: 'test/spec/*-spec.js', included: false},
+  	{pattern: 'libs/jquery/jquery.js', included: false},
+  	{pattern: 'libs/requirejs/require.js', included: false},
+  	{pattern: 'src/*.js', included: false},
+  	{pattern: 'test/spec/*-spec.js', included: false},
 
-  'test/test-main.js',
+  	// helpers & fixtures for jasmine-jquery
+  	{ pattern: 'test/helpers/*.js', included: true },
+
+  	'test/test-main.js',
 ];
 
 
