71eafcacf01408e2b26b7dbe5a5ca97829175a34
goliatone <burgosemi@gmail.com>
2014-02-22 19:39:03 -0500

    Example: Update main, expose content to console.

--- examples/main.js
+++ examples/main.js
@@ -9,14 +9,30 @@ requirejs.config({
 define(['gpub'], function (Gpub) {
     console.log('Loading ');
 	var gpub = new Gpub();
-	window.Gpub = Gpub;
 
-	var User = function(){};
+	
+
+	var User = function(name){this.name = name;};
 	Gpub.observable(User);
-	var pepe = new User();
+	
+	var pepe = new User('pepe');
+
 	pepe.on('change', function(){
-		console.log('We are here');
+		console.log('Hello, Im', this.name);
+	});
+
+	var pipo = new User('pipo');
+
+	pipo.on('change', function(){
+		console.log('Hello, Im', this.name);
 	});
 
 	pepe.emit('change');
+
+
+	window.Gpub = Gpub;
+	window.User = User;
+	window.pepe = pepe;
+	window.pipo = pipo;
+
 });
