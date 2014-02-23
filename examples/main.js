1fe5f4230d9300ca547fcc37ebeb83224e40e163
goliatone <burgosemi@gmail.com>
2014-02-22 20:10:27 -0500

    Example: Add Gpub.observable example.

--- examples/main.js
+++ examples/main.js
@@ -10,21 +10,34 @@ define(['gpub'], function (Gpub) {
     console.log('Loading ');
 	var gpub = new Gpub();
 
-	
 
-	var User = function(name){this.name = name;};
+
+	var User = function(name){
+		this.data = {};
+		this.data['name'] = name;
+	};
+	User.prototype.set = function(key, value){ this.data[key] = value; return this;};
+    User.prototype.get = function(key, def){ return this.data[key] || def; };
+            
 	Gpub.observable(User);
-	
+	Gpub.bindable(User.prototype, 'set', 'get');
+
 	var pepe = new User('pepe');
 
-	pepe.on('change', function(){
-		console.log('Hello, Im', this.name);
+	pepe.on('change', function(event){
+		console.log('Hello, Im', this.get('name'));
+		console.log(event);
+	});
+
+	pepe.on('change:name', function(event){
+		console.log('I have a new name', event.value);
 	});
 
 	var pipo = new User('pipo');
 
-	pipo.on('change', function(){
-		console.log('Hello, Im', this.name);
+	pipo.on('change', function(event){
+		console.log('Hello, Im', this.get('name'));
+		console.log(event);
 	});
 
 	pepe.emit('change');
